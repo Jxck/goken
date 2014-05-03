abd7dd78b9c05f3e8eeb79d56be19dd78414375b
Jxck <block.rxckin.beats@gmail.com>
2014-05-04 03:51:55 +0900

    add goken18

---
+++ goken18-orm/src/jxck/query/q.go
@@ -0,0 +1,99 @@
+package q
+
+import (
+	"fmt"
+	"log"
+	"strings"
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
+
+type STATEMENT string
+
+const (
+	SELECT STATEMENT = "SELECT"
+	FROM             = "FROM"
+	WHERE            = "WHERE"
+	ORDER            = "ORDER BY"
+	JOIN             = "JOIN"
+	ON               = "ON"
+)
+
+const (
+	COMMA = ","
+	SPACE = " "
+	ASC   = "ASC"
+	DESC  = "DESC"
+)
+
+func NewDB() *DB {
+	return &DB{
+		query: new(Query),
+	}
+}
+
+type DB struct {
+	query *Query
+}
+
+type Query string
+
+func (q *Query) Start(stmt STATEMENT, literal string) {
+	*q += Query(fmt.Sprintf("%s %s", stmt, literal))
+}
+
+func (q *Query) Append(stmt STATEMENT, literal string) {
+	*q += Query(fmt.Sprintf(" %s %s", stmt, literal))
+}
+func (q *Query) String() string {
+	return string(*q)
+}
+
+func (db *DB) Select(str ...string) *DB {
+	s := strings.Join(str, COMMA+SPACE)
+	db.query.Start(SELECT, s)
+	return db
+}
+
+func (db *DB) From(str string) *DB {
+	db.query.Append(FROM, str)
+	return db
+}
+
+func escape(value string) string {
+
+}
+
+func (db *DB) Where(str string, values ...string) *DB {
+	for _, v := range values {
+		str = strings.Replace(str, "?", escape(v), 1)
+	}
+	db.query.Append(WHERE, str)
+	return db
+}
+
+func (db *DB) Join(str string) *DB {
+	db.query.Append(JOIN, str)
+	return db
+}
+
+func (db *DB) On(str string) *DB {
+	db.query.Append(ON, str)
+	return db
+}
+
+func (db *DB) OrderAsc() *DB {
+	db.query.Append(ORDER, ASC)
+	return db
+}
+
+func (db *DB) OrderDesc() *DB {
+	db.query.Append(ORDER, DESC)
+	return db
+}
+
+func (db *DB) Query() *Query {
+	return db.query
+}
---
+++ goken18-orm/src/jxck/query/q_test.go
@@ -0,0 +1,44 @@
+package q
+
+import (
+	"jxck/assert"
+	"testing"
+)
+
+func TestSelect(t *testing.T) {
+	db := NewDB()
+	var actual string = db.Select("name", "age", "email").From("users").Query().String()
+	var expected string = "SELECT name, age, email FROM users"
+	assert.Equal(t, actual, expected)
+}
+
+func TestWhere(t *testing.T) {
+	db := &DB{
+		query: new(Query),
+	}
+	var actual string = db
+		.Select("name")
+		.From("users")
+		.Where("id = ? and age > ?", "1", "20")
+		.Query().String()
+	var expected string = "SELECT name FROM users WHERE id = 1 and age > 20"
+	assert.Equal(t, actual, expected)
+}
+
+func TestOrderDesc(t *testing.T) {
+	db := &DB{
+		query: new(Query),
+	}
+	var actual string = db.Select("name").From("users").Where("id = 1").OrderDesc().Query().String()
+	var expected string = "SELECT name FROM users WHERE id = 1 ORDER BY DESC"
+	assert.Equal(t, actual, expected)
+}
+
+func TestJoin(t *testing.T) {
+	db := &DB{
+		query: new(Query),
+	}
+	var actual string = db.Select("name").From("users").Where("id = 1").Join("depts").On("users.dept_id = depts.id").OrderDesc().Query().String()
+	var expected string = "SELECT name FROM users WHERE id = 1 JOIN depts ON users.dept_id = depts.id ORDER BY DESC"
+	assert.Equal(t, actual, expected)
+}
